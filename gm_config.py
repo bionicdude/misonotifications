d935041982925328b2890be9c420bf3b0f8cad68
Stuart <github@bionicdude.com>
2012-01-28 10:29:33 +0000

    config file with no keys present

---
+++ gm_config.py
@@ -0,0 +1,18 @@
+import os
+
+#my API key registered to my application
+consumer_key="jSvOQwxje1rbLcScecms"
+#my API secret (i trust you)
+consumer_secret="xxxxxxx"
+#your go miso username (email)
+gm_username="miso@bionicdude.com"
+#your gomiso password
+gm_password="xxxxx"
+#a token file (just leave this setting unless you really want the token in a different location)
+tokensFile=os.getcwd() + "/token"
+#the username for talking to XBMC
+gm_xbmcusername="bionicdude"
+#and the password
+gm_xbmcpassword="xxxxx"
+#and the address and port that you have it listening on
+gm_xbmcaddress="192.168.1.15:8084"
