c572e9a6ecf28005397dbde7a45995859238f1af
Stuart <github@bionicdude.com>
2012-01-29 00:33:08 +0000

    rearranged stuff to be more generic and added some more comments

--- gmnotifier.py
+++ gmnotifier.py
@@ -12,33 +12,39 @@ import json
 import urllib
 import os
 
-def BioNotify(bn_title="Bionic Notification",bn_msg="this is the message",bn_duration=5000):
-    bn_return=""
+def genNotify(gn_title="Notification",gn_msg="this is the message",gn_duration=5):
+    #This will be the generic notification that will be called throughout
+    #it will try to contact xbmc first...if it can't it will pass the message to pynotify.
+    global os    
+    gn_return=""
+    msg=urllib.quote(gn_msg)
+    #Hopefully that will have 'escaped' characters and encoded accordingly
+    urltoget="GET \"http://%s:%s@%s/xbmcCmds/xbmcHttp?command=ExecBuiltIn(Notification(%s, %s,7500))\"" % (gm_xbmcusername,gm_xbmcpassword,gm_xbmcaddress,gn_title,msg)
+    logger.debug("Sending xbmc msg via: %s" % urltoget)
+    xbmcsuccess= str(os.system(urltoget))
+    if "<li>OK" in xbmcsuccess:
+        return gn_return
+        exit
     try:
+       #will doing this import over and over eat memory, or is it clever enough to not? mmm
        import gtk, pygtk, os, os.path, pynotify
        pygtk.require('2.0')
     except:
-       bn_return = "Error: need python-notify, python-gtk2 and gtk"
+       gn_return = "Error: need python-notify, python-gtk2 and gtk"
     if not pynotify.init("Timekpr notification"):
         return "timekpr notification failed to initialize"
         sys.exit(1)
-    n = pynotify.Notification(bn_title, bn_msg,"file://%s/icon.png" % mypath)
+    n = pynotify.Notification(gn_title, gn_msg,"file://%s/icon.png" % mypath)
     #n = pynotify.Notification("Moo title", "test", "file:///path/to/icon.png")
     n.set_urgency(pynotify.URGENCY_CRITICAL)
-    n.set_timeout(bn_duration*1000) # 5 seconds
+    n.set_timeout(gn_duration*1000) # 5 seconds
     n.set_category("device")
-
-    #Call an icon
-    #helper = gtk.Button()
-    #icon = helper.render_icon(gtk.STOCK_DIALOG_WARNING, gtk.ICON_SIZE_DIALOG)
-    #n.set_icon_from_pixbuf(icon)
-
     if not n.show():
-        bn_return= "Failed to send notification"
-    return bn_return
+        gn_return= "Failed"
+    return gn_return
     
-def CreateLog(logname='BioLog',loglevel=logging.DEBUG):
-    logger=logging.getLogger('BioLog')
+def CreateLog(logname='GenLog',loglevel=logging.DEBUG):
+    logger=logging.getLogger('GenLog')
     logger.setLevel(loglevel)
     lh=logging.handlers.RotatingFileHandler("%s/%s.log" % (mypath,logname),maxBytes=256*1024,backupCount=9)
     lh.setLevel(loglevel)
@@ -48,26 +54,38 @@ def CreateLog(logname='BioLog',loglevel=logging.DEBUG):
     return logger
 
 def Startup():
+   #This is the "initialisation call"
+
+   #logger is the global logging object..
    global logger
+   #gm is our gomiso object (global here in case anyone else needed it, but maybe they won't)
    global gm
+   #a login oject to hold our authenticated user info
    global login
+   #mypath holds the location of the script so that relative references can be made
    global mypath
    mypath=sys.path[0]
-   print mypath
+   #create the logger object..
    logger=CreateLog("MisoNotifier")
+   #and the gomiso object..
    gm=gomiso.gomiso()
-   if gm.authentification(consumer_key, consumer_secret, gm_username, gm_password, tokensFile):
+   #try to authentiate with miso..
+   logger.info('trying to authenticate with miso to grab api handle')
+   loginsuccess = gm.authentification(consumer_key, consumer_secret, gm_username, gm_password, tokensFile)
+   try:
       login = json.loads(gm.getUserInfo())
+      #getting this far should meant that we were authorized, so we have populated our global login object with the json data
+      #and log our success :)
       logger.info ('logged in as ' + login['user']['username'])
-    
-def XBMCMsg(msg="weeee"):
-    msg=urllib.quote(msg)
-    #print msg
-    urltoget="GET \"http://%s:%s@%s/xbmcCmds/xbmcHttp?command=ExecBuiltIn(Notification(BioNotify, %s,7500))\"" % (gm_xbmcusername,gm_xbmcpassword,gm_xbmcaddress,msg)
-    logger.info("Sending xbmc msg via: %s" % urltoget)
-    return os.system(urltoget)
+   except:
+      logger.error('looks like we had issues logging in - check consumer_key,secret, username, password in config')
+      return 1
+      exit
+   return 0
 
-Startup()
+if Startup()>0:
+    logger.info("seeing as the startup failed..we'll just exit now")
+    sys.exit()
 myid=login['user']['id']
 curnews=""
 while True:
@@ -91,12 +109,9 @@ while True:
             ep_number=""
       newnews=username+ ": "+showname+" "+ep_number
       if curnews!=newnews:
-         aaa = XBMCMsg(newnews)
-         #print "result was: %s" % aaa
          logger.info(newnews)
          curnews = newnews#
-         if aaa!=0:
-            BioNotify(bn_msg=newnews)
+         genNotify(gn_title='Miso Notification',gn_msg=newnews)
    except Exception, e:
       logger.error(" %s" %e)
    time.sleep(120)
