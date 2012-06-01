9cc4daf77176ac4721409373117a814c71f6de5b
bionicdude <github@bionicdude.com>
2012-06-01 13:13:10 +0100

    once you've cloned the repo, use this to start (and update)

--- start.py
+++ start.py
@@ -2,6 +2,6 @@
 import os
 
 #Download the latest version..
-os.system("wget -O gmnotifier.py \"https://dl.dropbox.com/s/648593oubom8fe5/gmnotifier.py?dl=1\"")
+os.system("git pull")
 #And then run it..
 os.system("python gmnotifier.py")
