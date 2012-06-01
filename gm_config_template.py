460d0de9908e3b946ccc84161aaa445ffe31af5d
bionicdude <github@bionicdude.com>
2012-06-01 13:12:19 +0100

    edit this with your own details and name gm_config.py

---
+++ gm_config_template.py
@@ -0,0 +1,26 @@
+import os
+
+#my API key registered to my application
+consumer_key="jSvOQwxje1rbLcScecms"
+#my API secret (i trust you)
+consumer_secret="K23KjxW7r1MkObgCr2o9rXEdYuJkKmEj6Ab0WBqD"
+#your go miso username (email)
+gm_username="miso@bionicdude.com"
+#your gomiso password
+gm_password="xxxxx"
+#a token file (just leave this setting unless you really want the token in a different location)
+tokensFile=os.getcwd() + "/token"
+#the username for talking to XBMC
+gm_xbmcusername="bionicdude"
+#and the password
+gm_xbmcpassword="xxxxx"
+#and the address and port that you have it listening on
+gm_xbmcaddress="192.168.1.52:8084"
+#identity to ignore for tooltip (your gomiso name)
+gm_me='bionicdude'
+#number of rows you want to display in tooltip
+gm_tooltipsize=10
+#want to see the same show twice in a row if two people watch it?
+gm_repeatnotify=True
+#using unity?
+gm_UseUnity=False
