3388b9f2338b39d53f68e7d049ca2b552ca59615
bionicdude <github@bionicdude.com>
2012-06-13 13:05:05 +0100

    a little housekeeping/tidying

--- dbstuff.py
+++ dbstuff.py
@@ -1,5 +1,4 @@
 import os
-import datetime
 import sqlite3
 import logging
 import logging.handlers
@@ -8,95 +7,115 @@ from gm_config import *
 logger = logging.getLogger("MisoNotifier")
 
 class db:
-    def __init__(self):
-        if not os.path.exists('data'):
-            #create new DB, create table stocks
-            self.con = sqlite3.connect('data')
-            conn=self.con
-            conn.execute('create table usernames (id integer primary key, NickName text, alias_gomiso text);')
-            conn.execute('create table gomiso ( "When" integer, Who text, What text, notified integer);')
-            conn.execute('create table tmp_gomiso("When" integer, Who text, What text);')
-            conn.execute('create unique index usernames_gomiso on usernames(alias_gomiso);')
-            conn.execute('create index gomiso_when on gomiso("when");')
-            conn.execute('insert into usernames values (null,"bionicdude","bionicdude");')
-            conn.execute('insert into gomiso values (1234567890123456789,"placeholder","initial record",0);')
-            conn.commit()
-        else:
-            #use existing DB
-            self.con = sqlite3.connect('data')
-        self.cur = self.con.cursor()
-        self.con.row_factory = sqlite3.Row
+	def __init__(self):
+		if not os.path.exists('data'):
+			#create new DB, create table stocks
+			self.con = sqlite3.connect('data')
+			conn=self.con
+			conn.execute('create table usernames (id integer primary key, NickName text, alias_gomiso text, alias_lastfm text);')
+			conn.execute('create table gomiso ( "When" integer, Who text, What text, notified integer);')
+			conn.execute('create table tmp_gomiso("When" integer, Who text, What text);')
+			conn.execute('create unique index usernames_gomiso on usernames(alias_gomiso);')
+			conn.execute('create index gomiso_when on gomiso("when");')
+			conn.execute('insert into usernames values (null,"bionicdude","bionicdude","bionicdude");')
+			conn.execute('insert into gomiso values (1234567890123456789,"placeholder","initial record",0);')
+			conn.execute('create table lastfm ( "When" integer, Who text, What text, notified integer);')
+			conn.execute('create table tmp_lastfm("When" integer, Who text, What text);')
+			conn.execute('create unique index usernames_lastfm on usernames(alias_lastfm);')
+			conn.execute('create index lastfm_when on lastfm("when");')
+			conn.commit()
+		else:
+			#use existing DB
+			self.con = sqlite3.connect('data')
+		self.cur = self.con.cursor()
+		self.con.row_factory = sqlite3.Row
 
-    def updateshows(self):
-        logger.info("updating shows")
-        conn=self.con
-        conn.row_factory=sqlite3.Row
-        curs=conn.cursor()
-        curs.execute('select * from gomiso')
-	#only insert new programs for users...don't worry if they've watched the same thing again (ignore the when bit)
-        querystring='''
-insert into gomiso
-select a.*,0
-from tmp_gomiso a
-left outer join gomiso b
-on a.who=b.who and a.what=b.what
-where b.who is null;'''
-        conn.execute(querystring)
-        conn.commit()
-        conn.execute('delete from tmp_gomiso;')
-        conn.commit()
-        querystring='select * from gomiso left outer join usernames on who=alias_gomiso where nickname!="%s" or nickname is null order by "when" desc limit %s' % (gm_me,str(gm_tooltipsize))
-        #print querystring
-        curs.execute(querystring)
-        #print "What we have now\n"
-        sofar=curs.fetchall()
-        result = ''
-        for row in sofar:
-            result += "%s %s: %s\n" % (row["When"], row["Who"], row["What"])
-        logger.debug(result)
-        return result.strip()
-    def newshows(self):
-        result=''
-        logger.info("getting new shows")
-        conn=self.con
-        conn.row_factory=sqlite3.Row
-        curs=conn.cursor()
-        curs.execute('select * from gomiso where notified=0 order by "when" asc')
-        newshows=curs.fetchall()
-        for row in newshows:
-            result += "%s@%s@%s\n" % (row["When"], row["Who"], row["What"])
-        logger.debug(result)
-        logger.info('setting newshows as notified')
-        conn.execute('update gomiso set notified=1 where notified=0;')
-        return result.strip()
-    def TopXUsers(self,usercount):
-        logger.info("fetching top three users for webpage")
-        conn=self.con
-        conn.row_factory=sqlite3.Row
-        curs=conn.cursor()
-        curs.execute('select who,count(who) howmany from gomiso where upper(who)!="%s" group by who order by howmany desc limit %s' % (gm_me.upper(),str(usercount)))
-        sofar=curs.fetchall()
-        result = ''
-        for row in sofar:
-            result += "%s\n" % row["Who"]
-        logger.debug(result)
-        return result.strip()
-    def UserActivity(self,user="",noa=20):
-        if user.upper()!="EVERYONE":
-            noa=40
-            user='where upper(who)="%s"' % user
-        else:
-            user=""
-        conn=self.con
-        conn.row_factory=sqlite3.Row
-        curs=conn.cursor()
-        curs.execute('select "when", Who, What from gomiso %s order by "when" desc limit %s' % (user.upper(),str(noa)))
-        thelist=curs.fetchall()
-        result=''
-        for row in thelist:
-            if user=="":
-                result += '%s <stu>%s</stu> %s\n' % (row["when"],row["Who"],row["what"])
-            else:
-                result += '%s %s\n' % (row["when"],row["what"])
-        #print result
-        return result.strip()
+	def updateshows(self):
+		logger.info("updating shows")
+		conn=self.con
+		conn.row_factory=sqlite3.Row
+		curs=conn.cursor()
+		#curs.execute('select * from gomiso')
+		#only insert new programs for users...don't worry if they've watched the same thing again (ignore the when bit)
+		querystring='''
+		insert into gomiso
+		select a.*,0
+		from tmp_gomiso a
+		left outer join gomiso b
+		on a.who=b.who and a.what=b.what
+		where b.who is null;'''
+		conn.execute(querystring)
+		conn.commit()
+		conn.execute('delete from tmp_gomiso;')
+		conn.commit()
+		querystring='select * from gomiso left outer join usernames on upper(who)=upper(alias_gomiso) where upper(nickname)!="%s" or nickname is null order by "when" desc limit %s' % (gm_me.upper(),str(gm_tooltipsize))
+		#print querystring
+		curs.execute(querystring)
+		#print "What we have now\n"
+		sofar=curs.fetchall()
+		result = ''
+		for row in sofar:
+			result += "%s %s: %s\n" % (row["When"], row["Who"], row["What"])
+		logger.debug(result)
+		return result.strip()
+
+	def newshows(self,SetAsNotified=True):
+		result=''
+		logger.info("getting new shows")
+		conn=self.con
+		conn.row_factory=sqlite3.Row
+		curs=conn.cursor()
+		curs.execute('select * from gomiso where notified=0 order by "when" asc')
+		newshows=curs.fetchall()
+		for row in newshows:
+			result += "%s@%s@%s\n" % (row["When"], row["Who"], row["What"])
+		logger.debug(result)
+		if SetAsNotified:
+			logger.info('setting newshows as notified')
+			conn.execute('update gomiso set notified=1 where notified=0;')
+		return result.strip()
+
+	def TopXUsers(self,usercount):
+		logger.info("fetching top three users for webpage")
+		conn=self.con
+		conn.row_factory=sqlite3.Row
+		curs=conn.cursor()
+		curs.execute('select who,count(who) howmany from gomiso where upper(who)!="%s" group by who order by howmany desc limit %s' % (gm_me.upper(),str(usercount)))
+		sofar=curs.fetchall()
+		result = ''
+		for row in sofar:
+			result += "%s\n" % row["Who"]
+		logger.debug(result)
+		return result.strip()
+
+	def UserActivity(self,user="",noa=20):
+		if user.upper()!="EVERYONE":
+			noa=40
+			user='where upper(who)="%s"' % user
+		else:
+			user=""
+		conn=self.con
+		conn.row_factory=sqlite3.Row
+		curs=conn.cursor()
+		curs.execute('select "when", Who, What from gomiso %s order by "when" desc limit %s' % (user.upper(),str(noa)))
+		thelist=curs.fetchall()
+		result=''
+		for row in thelist:
+			if user=="":
+				result += '%s <stu>%s</stu> %s\n' % (row["when"],row["Who"],row["what"])
+			else:
+				result += '%s %s\n' % (row["when"],row["what"])
+		#print result
+		return result.strip()
+
+	def AddTempShowRecord(self,when="",who="", what=""):
+		conn=self.con
+		conn.row_factory=sqlite3.Row
+		curs=conn.cursor()
+		curs.execute('insert into tmp_gomiso values(?,?,?);', (when, who, what))
+
+	def AddTempFMRecord(self,when="",who="", what=""):
+		conn=self.con
+		conn.row_factory=sqlite3.Row
+		curs=conn.cursor()
+		curs.execute('insert into tmp_lastfm values(?,?,?);', (when, who, what))
--- gmnotifier.py
+++ gmnotifier.py
@@ -39,7 +39,7 @@ class SystrayIconApp:
 		global login
 		global doing
 		global tkinter
-		self.menu = gtk.Menu()		
+		self.menu = gtk.Menu()
 		if gm_UseUnity == False:
 			print "not using unity"
 			self.tray = gtk.status_icon_new_from_file("icon.png")
@@ -48,12 +48,11 @@ class SystrayIconApp:
 		else:
 			print "using unity"
 			import appindicator
-                	self.tray = appindicator.Indicator("ActivityNotifier", "gtk-execute", appindicator.CATEGORY_APPLICATION_STATUS)
-	    		self.tray.set_status (appindicator.STATUS_ACTIVE)
+			self.tray = appindicator.Indicator("ActivityNotifier", "gtk-execute", appindicator.CATEGORY_APPLICATION_STATUS)
+			self.tray.set_status (appindicator.STATUS_ACTIVE)
 			self.tray.set_attention_icon ("indicator-messages-new")
 			self.tray.set_icon(os.getcwd() + "/icon.png")
 			self.tray.set_menu(self.menu)
-	    	
 		if Startup()>0:
 			logger.info("seeing as the startup failed..we'll just exit now")
 			print "Error(see log)..Exiting"
@@ -70,7 +69,6 @@ class SystrayIconApp:
 		about.show()
 		about.connect('activate', self.show_about_dialog)
 
-        	
 		#miso window
 		if notkinter:
 			miso = gtk.MenuItem("install python-tk to use popup list")
@@ -79,13 +77,13 @@ class SystrayIconApp:
 		self.menu.append(miso)
 		miso.show()
 		miso.connect('activate', self.show_miso_list)
-        
+
 		# add quit item
 		quit = gtk.MenuItem("Quit")
 		quit.show()
 		self.menu.append(quit)
 		quit.connect('activate', gtk.main_quit)
-		
+
 	def on_right_click(self, icon, event_button, event_time):
 		self.menu.popup(None, None, gtk.status_icon_position_menu,
 		event_button, event_time, self.tray)
@@ -109,164 +107,164 @@ class SystrayIconApp:
 			#we won't even bother writing debug..
 			pass
 	def show_miso_list(self, widget):
-            try:
-                msgbox(shows)
-            except:
-                pass
+		try:
+			msgbox(shows)
+		except:
+			pass
 
 def genNotify(gn_title="Notification",gn_msg="this is the message",gn_duration=5):
-    #This will be the generic notification that will be called throughout
-    #it will try to contact xbmc first...if it can't it will pass the message to pynotify.
-    global os
-    global logger
-    gn_return=""
-    msg=urllib.quote(gn_msg)
-    #Hopefully that will have 'escaped' characters and encoded accordingly
-    urltoget="GET \"http://%s:%s@%s/xbmcCmds/xbmcHttp?command=ExecBuiltIn(Notification(%s, %s,7500))\"" % (gm_xbmcusername,gm_xbmcpassword,gm_xbmcaddress,gn_title,msg)
-    #logger.debug("Sending xbmc msg via: %s" % urltoget)
-    try:
-     xbmcsuccess= str(os.system(urltoget))
-    except:
-     logger.info("xbmc probably not running (although that in itself shouldn't give me an error..)")
-    #logger.debug(xbmcsuccess)
-    if ("<li>OK" in xbmcsuccess) or str(xbmcsuccess)=="0":
-        return gn_return
-        exit
-    try:
-       #logger.debug("importing py stuff")
-       #will doing this import over and over eat memory, or is it clever enough to not? mmm
-       import gtk, pygtk, os, os.path, pynotify
-       pygtk.require('2.0')
-    except:
-       gn_return = "Error: need python-notify, python-gtk2 and gtk"
-    if not pynotify.init("Timekpr notification"):
-        return "timekpr notification failed to initialize"
-        sys.exit(1)
-    n = pynotify.Notification(gn_title, gn_msg,"file://%s/icon.png" % mypath)
-    #n = pynotify.Notification("Moo title", "test", "file:///path/to/icon.png")
-    n.set_urgency(pynotify.URGENCY_LOW)
-    n.set_timeout(gn_duration*1000) # 5 seconds
-    n.set_category("device")
-    if not n.show():
-        gn_return= "Failed"
-    return gn_return
+	#This will be the generic notification that will be called throughout
+	#it will try to contact xbmc first...if it can't it will pass the message to pynotify.
+	global os
+	global logger
+	gn_return=""
+	msg=urllib.quote(gn_msg)
+	#Hopefully that will have 'escaped' characters and encoded accordingly
+	urltoget="GET \"http://%s:%s@%s/xbmcCmds/xbmcHttp?command=ExecBuiltIn(Notification(%s, %s,7500))\"" % (gm_xbmcusername,gm_xbmcpassword,gm_xbmcaddress,gn_title,msg)
+	#logger.debug("Sending xbmc msg via: %s" % urltoget)
+	try:
+		xbmcsuccess= str(os.system(urltoget))
+	except:
+		logger.info("xbmc probably not running (although that in itself shouldn't give me an error..)")
+	#logger.debug(xbmcsuccess)
+	if ("<li>OK" in xbmcsuccess) or str(xbmcsuccess)=="0":
+		return gn_return
+		exit
+	try:
+		#logger.debug("importing py stuff")
+		#will doing this import over and over eat memory, or is it clever enough to not? mmm
+		import gtk, pygtk, os, os.path, pynotify
+		pygtk.require('2.0')
+	except:
+		gn_return = "Error: need python-notify, python-gtk2 and gtk"
+	if not pynotify.init("Timekpr notification"):
+		return "timekpr notification failed to initialize"
+		sys.exit(1)
+	n = pynotify.Notification(gn_title, gn_msg,"file://%s/icon.png" % mypath)
+	#n = pynotify.Notification("Moo title", "test", "file:///path/to/icon.png")
+	n.set_urgency(pynotify.URGENCY_LOW)
+	n.set_timeout(gn_duration*1000) # 5 seconds
+	n.set_category("device")
+	if not n.show():
+		gn_return= "Failed"
+	return gn_return
 
 def CreateLog(logname='GenLog',loglevel=logging.DEBUG):
-    logger=logging.getLogger(logname)
-    logger.setLevel(loglevel)
-    lh=logging.handlers.RotatingFileHandler("%s/%s.log" % (mypath,logname),maxBytes=256*1024,backupCount=9)
-    lh.setLevel(loglevel)
-    formatter = logging.Formatter('%(asctime)s - %(levelname)s - %(message)s')
-    lh.setFormatter(formatter)
-    logger.addHandler(lh)
-    return logger
+	logger=logging.getLogger(logname)
+	logger.setLevel(loglevel)
+	lh=logging.handlers.RotatingFileHandler("%s/%s.log" % (mypath,logname),maxBytes=256*1024,backupCount=9)
+	lh.setLevel(loglevel)
+	formatter = logging.Formatter('%(asctime)s - %(levelname)s - %(message)s')
+	lh.setFormatter(formatter)
+	logger.addHandler(lh)
+	return logger
 
 def Startup():
-   #This is the "initialisation call"
+	#This is the "initialisation call"
 
-   #logger is the global logging object..
-   global logger
-   #gm is our gomiso object (global here in case anyone else needed it, but maybe they won't)
-   global gm
-   #a login oject to hold our authenticated user info
-   global login
-   #mypath holds the location of the script so that relative references can be made
-   global mypath
-   mypath=sys.path[0]
-   #create the logger object..
-   logger=CreateLog("MisoNotifier")
-   #and the gomiso object..
-   gm=gomiso.gomiso()
-   #try to authentiate with miso..
-   logger.info('trying to authenticate with miso to grab api handle')
-   loginsuccess = gm.authentification(consumer_key, consumer_secret, gm_username, gm_password, tokensFile)
-   try:
-      login = json.loads(gm.getUserInfo())
-      #getting this far should meant that we were authorized, so we have populated our global login object with the json data
-      #and log our success :)
-      logger.info ('logged in as ' + login['user']['username'])
-   except:
-      logger.error('looks like we had issues logging in - check consumer_key,secret, username, password in config')
-      return 1
-      exit
-   return 0
+	#logger is the global logging object..
+	global logger
+	#gm is our gomiso object (global here in case anyone else needed it, but maybe they won't)
+	global gm
+	#a login oject to hold our authenticated user info
+	global login
+	#mypath holds the location of the script so that relative references can be made
+	global mypath
+	mypath=sys.path[0]
+	#create the logger object..
+	logger=CreateLog("MisoNotifier")
+	#and the gomiso object..
+	gm=gomiso.gomiso()
+	#try to authentiate with miso..
+	logger.info('trying to authenticate with miso to grab api handle')
+	loginsuccess = gm.authentification(consumer_key, consumer_secret, gm_username, gm_password, tokensFile)
+	try:
+		login = json.loads(gm.getUserInfo())
+		#getting this far should meant that we were authorized, so we have populated our global login object with the json data
+		#and log our success :)
+		logger.info ('logged in as ' + login['user']['username'])
+	except:
+		logger.error('looks like we had issues logging in - check consumer_key,secret, username, password in config')
+		return 1
+		exit
+	return 0
 
 def mainprogloop():
- global curnews
- global curuser
- global myid
- global gm
- global logger
- global oh
- global initialrun
- global shows
- if True:
-   print "loop"
-   try:
-      if initialrun:
-         feedcount=30
-      else:
-         feedcount=10
-      feed=json.loads(gm.userHomeFeed(myid,feedcount))
-      #print feed[0]
-      for line in feed:
-         created_at=parse_timestamp(line['feed_item']['created_at'].encode("utf-8"))
-         showname=line['feed_item']['topics']['media']['title'].encode("utf-8")
-         username=line['feed_item']['user']['username'].encode("utf-8")
-         #logger.info(line)
-         if line['feed_item']['topics']['media']['kind']=='TvShow':
-            if "badge" in str(line):
-               ep_number="badge aquired: %s" % line['feed_item']['topics']['badge']['tagline']
-            else:
-               if "episode" in str(line):
-                  ep_number=line['feed_item']['topics']['episode']['label']
-               else:
-                  ep_number=""
-         else:
-            ep_number=""
-         #print created_at, username, showname, ep_number
-         mydata.cur.execute('insert into tmp_gomiso values(?,?,?);', (created_at,username,showname + " " + ep_number))
+	global curnews
+	global curuser
+	global myid
+	global gm
+	global logger
+	global oh
+	global initialrun
+	global shows
+	if True:
+		print "loop"
+		try:
+			if initialrun:
+				feedcount=30
+			else:
+				feedcount=10
+			feed=json.loads(gm.userHomeFeed(myid,feedcount))
+			#print feed[0]
+			for line in feed:
+				created_at=parse_timestamp(line['feed_item']['created_at'].encode("utf-8"))
+				showname=line['feed_item']['topics']['media']['title'].encode("utf-8")
+				username=line['feed_item']['user']['username'].encode("utf-8")
+				#logger.info(line)
+				if line['feed_item']['topics']['media']['kind']=='TvShow':
+					if "badge" in str(line):
+						ep_number="badge aquired: %s" % line['feed_item']['topics']['badge']['tagline']
+					else:
+						if "episode" in str(line):
+							ep_number=line['feed_item']['topics']['episode']['label']
+						else:
+							ep_number=""
+				else:
+					ep_number=""
+				#print created_at, username, showname, ep_number
+				mydata.AddTempShowRecord(created_at,username,showname + " " + ep_number)
 
-      newnews=str(created_at) +": "+showname+" "+ep_number
-      if gm_repeatnotify==False:
-         curuser=username
-      print "current news(%s) and newnews(%s)" % (curnews, newnews)
-      if curnews!=newnews or username!=curuser:
-         updatedshows=mydata.updateshows()
-         newshows=mydata.newshows().split('\n')
-         for show in newshows:
-            items=show.split('@')
-            if not items[1]==gm_me:
-                logger.info(items[0] +' '+ items[1] +' '+items[2])
-                genNotify(gn_title=items[1],gn_msg=items[2])
-         shows=mydata.updateshows()
-         oh.set_tooltip((shows))
-         curnews = newnews
-         curuser = username
-         #last thing
-   except Exception, e:
-      logger.error("%s" % e)
- return True
+			newnews=str(created_at) +": "+showname+" "+ep_number
+			if gm_repeatnotify==False:
+				curuser=username
+			print "current news(%s) and newnews(%s)" % (curnews, newnews)
+			if curnews!=newnews or username!=curuser:
+				updatedshows=mydata.updateshows()
+				newshows=mydata.newshows().split('\n')
+				for show in newshows:
+					items=show.split('@')
+					if not items[1]==gm_me:
+						logger.info(items[0] +' '+ items[1] +' '+items[2])
+						genNotify(gn_title=items[1],gn_msg=items[2])
+				shows=mydata.updateshows()
+				oh.set_tooltip((shows))
+				curnews = newnews
+				curuser = username
+				#last thing
+		except Exception, e:
+			logger.error("%s" % e)
+	return True
 
 initialrun=True
 if __name__ == "__main__":
-   oh=SystrayIconApp()
-   shows=mydata.updateshows()
-   showlist=shows.split('\n')
-   a = showlist[0]
-   b = a[20:]
-   c = b[:b.find(':')]
-   d = b[b.find(':')+2:]
-   #showlist[0][21:][:showlist[0][21:].find(':')]
-   genNotify(gn_title=c,gn_msg=d)
-   oh.set_tooltip((shows.strip()))
-   cherrypy.server.socket_host = gm_bindip
-   cherrypy.server.socket_port = gm_bindport
-   cherrypy.tree.mount(webs.Root(),config={
+	oh=SystrayIconApp()
+	shows=mydata.updateshows()
+	showlist=shows.split('\n')
+	a = showlist[0]
+	b = a[20:]
+	c = b[:b.find(':')]
+	d = b[b.find(':')+2:]
+	#showlist[0][21:][:showlist[0][21:].find(':')]
+	genNotify(gn_title=c,gn_msg=d)
+	oh.set_tooltip((shows.strip()))
+	cherrypy.server.socket_host = gm_bindip
+	cherrypy.server.socket_port = gm_bindport
+	cherrypy.tree.mount(webs.Root(),config={
 '/':
 {'tools.staticdir.on':True,
 'tools.staticdir.dir':current_dir
 }
 })
-   thread.start_new_thread(cherrypy.engine.start,())
-   gtk.main()
+	thread.start_new_thread(cherrypy.engine.start,())
+	gtk.main()
